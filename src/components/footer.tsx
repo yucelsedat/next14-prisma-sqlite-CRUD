07d8177e8be9ec9c404226b9e702dc5d1ffff32e
yucelsedat <yucelsedatt@gmail.com>
2025-01-15 16:51:46 +0300

    save

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='text-center mt-auto text-zinc-400 py-4 border-t'>
-      <small>&copy; 2025. All right reserved. </small>
+      <small>&copy; 2025 <span className='font-semibold'>Sedat Yucel</span>. All right reserved. </small>
     </footer>
   )
 }
